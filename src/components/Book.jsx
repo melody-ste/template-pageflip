663f9b84e479aa99aeed8a375fdc59a961ef1dc5
melody-ste <melody_stephan@icloud.com>
2026-01-19 23:57:21 +0100

    adding buttons to flip pages

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,19 +1,23 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import HTMLFlipBook from "react-pageflip";
 import Page from "./Page";
 
 export default function Book() {
 
+  const bookRef = useRef(null);
   const [size, setSize] = useState({ w: 350, h: 500 });
 
   useEffect(() => {
     const updateSize = () => {
-      const isMobile = window.innerWidth < 768;
+    const isMobile = window.innerWidth < 768;
 
-      const w = isMobile ? window.innerWidth * 0.98 : Math.min(window.innerWidth * 0.9, 350);
-      const h = w * 1.42;
-      setSize({ w, h });
-    };
+    const pageWidth = isMobile ? window.innerWidth * 0.98 : Math.min(window.innerWidth * 0.45, 420);
+
+    const pageHeight = pageWidth * 1.42;
+
+    setSize({ w: pageWidth, h: pageHeight });
+  };
+
 
     updateSize();
     window.addEventListener("resize", updateSize);
@@ -21,19 +25,31 @@ export default function Book() {
   }, []);
 
   return (
-    <HTMLFlipBook
-      width={size.w}
-      height={size.h}
-      size="fixed"
-      maxShadowOpacity={0.5}
-      showCover
-      mobileScrollSupport
-    >
-      <Page number={1}>Couverture</Page>
-      <Page number={2}>Page 1</Page>
-      <Page number={3}>Page 2</Page>
-      <Page number={4}>Page 3</Page>
-      <Page number={5}>Dos</Page>
-    </HTMLFlipBook>
+    <>
+      <div className="book-container">
+        <HTMLFlipBook
+          ref={bookRef}
+          width={size.w}
+          height={size.h}
+          size="fixed"
+          maxShadowOpacity={0.5}
+          showCover
+          mobileScrollSupport
+          usePortrait={false} 
+        >
+          <Page number={1}>Couverture</Page>
+          <Page number={2}>Page 1</Page>
+          <Page number={3}>Page 2</Page>
+          <Page number={4}>Page 3</Page>
+          <Page number={5}>Dos</Page>
+        </HTMLFlipBook>
+
+        <div className="book-controls">
+          <button onClick={() => bookRef.current.pageFlip().flipPrev()}>←</button>
+          <button onClick={() => bookRef.current.pageFlip().flipNext()}>→</button>
+        </div>
+      </div>
+    </>
+    
   );
 }
